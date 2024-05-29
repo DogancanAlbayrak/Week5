48dd3d1b4995436c899b41e6cd706c7db22f552d
DogancanAlbayrak <165707015+DogancanAlbayrak@users.noreply.github.com>
2024-05-29 13:11:22 +0300

    PatikaStore added

---
+++ src/Week5/PatikaStore/Operation.java
@@ -0,0 +1,12 @@
+package Week5.PatikaStore;
+
+public interface Operation {
+    public void runMenu();
+    public void showProductList();
+    public void addProduct();
+    public void deleteProduct();
+    public void filterByProductBrand();
+    public void filterByProductID();
+
+
+}
